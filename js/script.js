85bb6a3876c81a793a130ac9328807f2a9ebdc56
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-01-22 12:36:30 +0200

    change long functional to regexp

--- js/script.js
+++ js/script.js
@@ -298,11 +298,15 @@ document.addEventListener("DOMContentLoaded", () => {
     indicators.push(dot);
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
+
   next.addEventListener('click', () => {
-    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset === deleteNotDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -325,9 +329,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   prev.addEventListener('click', () => {
     if (offset === 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -353,7 +357,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset =  +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset =  deleteNotDigits(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -366,5 +370,5 @@ document.addEventListener("DOMContentLoaded", () => {
         current.textContent = slideIndex;
       }
     });
-  })
+  });
 });
